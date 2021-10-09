1d3e4cc1c46fb287f400e70b58cdca8b813256f5
khschimek <khschimek@mavs.coloradomesa.edu>
2021-10-08 18:45:48 -0600

    project 1

--- Project 1/actions.js
+++ Project 1/actions.js
@@ -1,11 +1,39 @@
-function speak(){
-    alert("Hello")
+function home1(){
+    alert("I Hope You Like Animals")
 }
 
-function speech(){
-    alert("Talk")
+function dog1(){
+    alert("Dalmation")
 }
 
-function spoke(){
-    alert("Goodbye")
+function dog2(){
+    alert("Chihuahua")
+}
+
+function dog3(){
+    alert("German Shepherd")
+}
+
+function cat1(){
+    alert("Calico")
+}
+
+function cat2(){
+    alert("Siamese")
+}
+
+function cat3(){
+    alert("Munchkin")
+}
+
+function bird1(){
+    alert("Penguin")
+}
+
+function bird2(){
+    alert("Grouse")
+}
+
+function bird3(){
+    alert("Ostrich")
 }
