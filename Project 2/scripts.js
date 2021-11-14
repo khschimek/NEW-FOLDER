faf41ab74956a7939d02959d16e89c45f40b3861
khschimek <khschimek@mavs.coloradomesa.edu>
2021-11-14 02:03:08 -0700

    project 2

--- Project 2/scripts.js
+++ Project 2/scripts.js
@@ -13,18 +13,60 @@ async function makeAPIRequest(requestURL) {
 }
 
 function getdog() {
+    getdog1()
+    getdog2()
+    getdog3()
+}
 
-    console.log("getting dog image")
+function getdog1() {
+
+    console.log("getting dog1 image")
 
     var URL = "https://dog.ceo/api/breeds/image/random"
 
     makeAPIRequest(URL).then(response => {
 
-        console.log("Full Response: ")
+        console.log("Full Response 1: ")
         console.log(response)
 
-        console.log("finished making API request")
+        console.log("finished making API request 1")
 
-        document.getElementById("dog").src = response.message
+        document.getElementById("dog1").src = response.message
     })
 }
+
+function getdog2() {
+
+    console.log("getting dog2 image")
+
+    var URL = "https://dog.ceo/api/breeds/image/random"
+
+    makeAPIRequest(URL).then(response => {
+
+        console.log("Full Response 2: ")
+        console.log(response)
+
+        console.log("finished making API request 2")
+
+        document.getElementById("dog2").src = response.message
+    })
+}
+
+function getdog3() {
+
+    console.log("getting dog3 image")
+
+    var URL = "https://dog.ceo/api/breeds/image/random"
+
+    makeAPIRequest(URL).then(response => {
+
+        console.log("Full Response 3: ")
+        console.log(response)
+
+        console.log("finished making API request 3")
+
+        document.getElementById("dog3").src = response.message
+    })
+}
+
+getdog()
